85fad94d1a8d86c727cfdbf775aff9c09fe53b71
Natalia <nataliaymiss@gmail.com>
2023-03-13 16:15:30 +0300

    2 commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace _1_лаба
 {
-    internal class Program
+        internal class Program
     {
         static void Main(string[] args)
         {
+            Dog dog = new Dog ();
         }
     }
 }
